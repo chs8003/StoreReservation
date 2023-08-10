d72d257686b53d0cae6365540fafee652018ada8
ccc9003 <chsshc_@naver.com>
2023-08-10 12:39:44 +0900

    각 가게 메뉴 VO

---
+++ 230608/Report3-1/src/main/java/com/hyeonsik/boot/vo/MenuVo.java
@@ -0,0 +1,12 @@
+package com.hyeonsik.boot.vo;
+
+import lombok.Data;
+
+@Data
+public class MenuVo {
+
+    private String menuName;
+    private String menuPrice;
+    private String menusavedNm;
+}
+ 
